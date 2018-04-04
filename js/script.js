1702125f41ebaf1cab963517d3d6022cf2fe1918
Zim80 <zim@bobru.net>
2018-04-04 14:26:44 +0300

    правки по js

---
+++ js/script.js
@@ -0,0 +1,68 @@
+var link = document.querySelector(".button-contact-red");
+var popup = document.querySelector(".modal-wright-us");
+var close = popup.querySelector(".modal-close");
+
+var form = popup.querySelector(".wright-form");
+var fio = popup.querySelector("[name=first-name]");
+var mail = popup.querySelector("[name=your-email]");
+var typetext = popup.querySelector("#wright-area");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("fio");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+
+  if (storage) {
+    fio.value = storage;
+    mail.focus();
+  } else {
+  fio.focus();
+}
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!fio.value || !mail.value|| !typetext.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+    console.log("Нужно ввести имя, емейл и текст сообщения");
+
+  } else {
+    if (isStorageSupport) {
+    localStorage.setItem("fio", fio.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
+  /* e.preventDefault(), popup.classList.add("modal-show"), fio.focus()
+}), close.addEventListener("click", function(e) {
+    e.preventDefault(), popup.classList.remove("modal-show")
+}), form.addEventListener("submit", function(e) {
+    fio.value && mail.value || (e.preventDefault(), console.log("Не введены имя и электронная почта "))
+}), window.addEventListener("keydown", function(e) {
+    27 === e.keyCode && (e.preventDefault(), popup.classList.contains("modal-show") && popup.classList.remove("modal-show"))
+}); */
